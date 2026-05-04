351a19b85522146d4f2f5e94111c37c775a24745
JuanPabloRoman-utn <juroman@frba.utn.edu.ar>
2026-05-03 22:59:35 -0300

    Resuelve conflicto en esPar: conserva version con operador por favor que ande

--- operaciones.c
+++ operaciones.c
@@ -18,8 +18,6 @@ int multiplicar(int a, int b) {
     return a * b;
 }
 
-
-
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 int esPar(int n) {
     return (n % 2) == 0; /* version main */
